20345687d2f891c6f3be1db444a26abba77e2998
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-07-03 18:14:40 -0700

    updated longest subsequence

--- longestSubsequence.js
+++ longestSubsequence.js
@@ -17,19 +17,19 @@ function lcs(s, i, r, j)
 
 function helper(s, i, r, j, cache)
 {
-	if (i == s.length || j == r.length)
+	if (i == 0 || j == 0)
 		return 0
 	if (cache[i][j] == -1)
 	{
-		if (s[i] == r[j])
+		if (s[i - 1] == r[j - 1])
 		{
-			cache[i][j] = 1 + lcs(s, i + 1, r, j + 1)
+			cache[i][j] = 1 + lcs(s, i - 1, r, j - 1)
 		}
 		else
 		{
 			cache[i][j] = Math.max(
-				lcs(s, i + 1, r, j),
-				lcs(s, i, r, j + 1)
+				lcs(s, i - 1, r, j),
+				lcs(s, i, r, j - 1)
 			)
 		}
 
@@ -39,12 +39,12 @@ function helper(s, i, r, j, cache)
 
 function lcsFast(s, r)
 {
-	let cache = new Array(s.length);
-	for (let i = 0; i < s.length; i++) {
-		cache[i] = new Array(r.length);
+	let cache = new Array(s.length + 1);
+	for (let i = 0; i < s.length + 1; i++) {
+		cache[i] = new Array(r.length + 1);
 		cache[i].fill(-1)
 	}
-	return helper(s, 0, r, 0, cache)
+	return helper(s, s.length, r, r.length, cache)
 }
 
 console.time("lcs")
