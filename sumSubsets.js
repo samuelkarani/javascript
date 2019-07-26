914e15a6a340b1c3dc302c1ec87410e8c00b7cd4
Samuel Karani <samuel.karani@berkeley.edu>
2019-07-25 20:58:08 -0700

    added solutions

--- sumSubsets.js
+++ sumSubsets.js
@@ -1,25 +1,17 @@
-function sumSubsets(A, v, res, i = A.length - 1)
+function sumSubsets(A, v, res = [], i = A.length - 1)
 {
     if (v == 0)
-        return 1
+        return console.log(res)
     if (i == -1)
-        return 0
-    let a, b
-    a = sumSubsets(A, v, res, i - 1)
-    if (a)
-        return 1
+        return
+    sumSubsets(A, v, res, i - 1)
     if (v >= A[i])
     {
         res.push(A[i])
-        b = sumSubsets(A, v - A[i], res, i - 1)
-        if (b)
-            return 1
-        else
-            res.pop()
+        sumSubsets(A, v - A[i], res, i - 1)
+        res.pop()
     }
     return 0
 }
 
-let res = []
-sumSubsets([5, 10, 12, 13, 15, 18], 30, res)
-console.log(res)
+sumSubsets([5, 10, 12, 13, 15, 18], 30)
